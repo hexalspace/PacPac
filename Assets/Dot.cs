13174543e71e4b6f569e02a60be222cf16ffaae5
Jacob Jarecki <jsjwooowooo@gmail.com>
2021-10-17 11:09:21 -0700

    WIP pre docky try out

--- Assets/Dot.cs
+++ Assets/Dot.cs
@@ -19,6 +19,6 @@ public class Dot : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("TrigHIT");
+
     }
 }
--- Assets/Pac.cs
+++ Assets/Pac.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Direction { LEFT, RIGHT, UP, DOWN }
 public class Pac : MonoBehaviour
 {
+
     public Animator a;
+    public Collider2D boxCollider;
+    public bool lastLeftRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,25 +19,95 @@ public class Pac : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var speed = 1;
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        bool hasMoved = false;
+        if (!hasMoved && Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += transform.right * Time.deltaTime * speed;
+            hasMoved = tryMove(Direction.RIGHT);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (!hasMoved && Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += -transform.right * Time.deltaTime * speed;
+            hasMoved = tryMove(Direction.LEFT);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (!hasMoved && Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += transform.up * Time.deltaTime * speed;
+            hasMoved = tryMove(Direction.UP);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (!hasMoved && Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += -transform.up * Time.deltaTime * speed;
+            hasMoved = tryMove(Direction.DOWN);
         }
+
+
+
     }
+
+    Vector2 nearestCenterPoint()
+    {
+        return Vector2.zero;
+    }
+
+    bool tryMove(Direction d)
+    {
+        //var nearestCenter = nearestCenterPoint();
+        //bool canChangeDirection = true;
+        //bool didChangeDirection = false;
+
+        //if (lastLeftRight && d == Direction.UP || d == Direction.DOWN && canChangeDirection)
+        //{
+        //    didChangeDirection = true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
+
+        //if (!lastLeftRight && d == Direction.LEFT || d == Direction.RIGHT && canChangeDirection)
+        //{
+        //    didChangeDirection = true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
+
+
+        var originalPosition = transform.position;
+        var speed = 1.0f;
+        var directionVector = Vector3.zero;
+        if (d == Direction.RIGHT)
+        {
+            directionVector = transform.right;
+        }
+        if (d == Direction.LEFT)
+        {
+            directionVector = -transform.right;
+        }
+        if (d == Direction.UP)
+        {
+            directionVector = transform.up;
+        }
+        if (d == Direction.DOWN)
+        {
+            directionVector = -transform.up;
+        }
+
+        transform.position += directionVector * Time.deltaTime * speed;
+
+        var results = new List<RaycastHit2D>();
+        Physics2D.Raycast(transform.position, directionVector, new ContactFilter2D(), results, 0.5f);
+        foreach(var r in results)
+        {
+            if (r.collider.gameObject.CompareTag("Wall")){
+                transform.position = originalPosition;
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
